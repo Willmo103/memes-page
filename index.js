8310fa58a53187773c4bf4c5f0074796cf89725b
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2022-09-16 09:08:10 -0500

    save meme functional

--- index.js
+++ index.js
@@ -1,86 +1,94 @@
-
 function getMemes(limit = 25, skip = 175) {
-    let memeArr = []
-    fetch(`http://192.168.1.208/memes/?limit=${limit}&skip=${skip}`, {
-        method: "GET",
+  let memeArr = [];
+  fetch(`http://192.168.1.208/memes/?limit=${limit}&skip=${skip}`, {
+    method: "GET",
+  })
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
     })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((res) => {
-            // console.log(res[0].url);
-            for (let i = 0; i < res.length; i++) {
-                memeArr.push(res[i].url);
-                // console.log(res[i].url)
-            }
-        });
-    // console.log(typeof (memeArr))
-    return memeArr
-
+    .then(function (data) {
+      for (i in data) {
+        memeArr.push(data[i]);
+      }
+      return memeArr;
+    });
+  return memeArr;
 }
 
 function logout() {
-    localStorage.clear()
+  localStorage.clear();
 }
 
 window.addEventListener("load", function () {
-    let memes = getMemes()
-    let token = localStorage.getItem("token")
-    let current = 0
-    let skip = 175
-    const img = document.getElementById("image")
-    const backBtn = document.getElementById("back")
-    const nextBtn = document.getElementById("next")
-    const linksDiv = document.getElementById("links")
+  let memes = getMemes();
+  let token = localStorage.getItem("token");
+  let current = 0;
+  let skip = 175;
+  const img = document.getElementById("image");
+  const backBtn = document.getElementById("back");
+  const nextBtn = document.getElementById("next");
+  const linksDiv = document.getElementById("links");
+  const saveBtn = document.getElementById("save");
 
-    if (token) {
-        console.log("token:", token);
-        linksDiv.innerHTML = `
+  if (token) {
+    linksDiv.innerHTML = `
         <li class="nav-item">
         <a class="nav-link" id="myMemesLink" href="mymemes.html">My Memes</a>
         </li>
         <li class="nav-item">
         <a class="nav-link" id="logout" href="" onclick="logout()">Logout</a>
-        </li>`
+        </li>`;
+  } else {
+    linksDiv.innerHTML = `<a class="nav-link" id="loginLink" href="login.html">Login</a>`;
+  }
+
+  setTimeout(function () {
+    img.src = memes[current].url;
+  }, 1000);
+
+  backBtn.addEventListener("click", function () {
+    current -= 1;
+    if (current < 0) {
+      current = 0;
     } else {
-        linksDiv.innerHTML = `<a class="nav-link" id="loginLink" href="login.html">Login</a>`
+      img.src = memes[current].url;
+    }
+  });
+
+  nextBtn.addEventListener("click", function () {
+    current += 1;
+    if (current >= memes.length - 1) {
+      let limit = 25;
+      skip += 25;
+      let moreMemes = getMemes(limit, skip);
+      setTimeout(function () {
+        memes = memes.concat(moreMemes);
+      }, 1000);
     }
 
-    setTimeout(function () {
-        // console.log(typeof (memes))
-        // console.log(memes)
-        img.src = memes[current]
-    }, 1000)
+    if (current !== memes.length) {
+      img.src = memes[current].url;
+    }
+  });
 
-    backBtn.addEventListener("click", function () {
-        current -= 1;
-        if (current < 0) {
-            current = 0;
-        } else {
-            img.src = memes[current]
-        }
+  saveBtn.addEventListener("click", function () {
+    let memeId = memes[current].id;
+    console.log(memeId);
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Origin": "*",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    };
+    fetch(`http://192.168.1.208/save/${memeId}`, {
+      method: "POST",
+      headers: headers,
+    }).then(function (response) {
+      if (response.ok) {
+        window.alert("Meme Saved!");
+      }
     });
-
-    nextBtn.addEventListener("click", function () {
-        current += 1
-        // console.log(current)
-        if (current >= memes.length - 1) {
-            let limit = 25
-            skip += 25
-            let moreMemes = getMemes(limit, skip)
-            setTimeout(function () {
-                memes = memes.concat(moreMemes)
-            }, 1000)
-            // console.log(memes)
-        };
-
-        if (current !== memes.length) {
-            img.src = memes[current];
-        };
-
-    });
-
+  });
 });
--- login.js
+++ login.js
@@ -1,36 +1,52 @@
 window.addEventListener("load", () => {
-    const form = document.getElementById("login")
+  const form = document.getElementById("login");
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        let email = document.querySelector("input[name=email]").value
-        let password = document.querySelector("input[name=password]").value
-        let newUser = document.querySelector("input[name=newUser]")
-        let data = JSON.stringify({ "email": email, "password": password })
-        console.log(data)
-        const headers = {
-            "Content-Type": "application/json",
-            "Access-Control-Origin": "*"
-        }
+    let email = document.querySelector("input[name=email]").value;
+    let password = document.querySelector("input[name=password]").value;
+    let newUser = document.querySelector("input[name=newUser]");
+    let data = JSON.stringify({ email: email, password: password });
+    console.log(data);
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Origin": "*",
+    };
 
-        fetch("http://192.168.1.208/login/", {
-            method: "POST",
-            headers: headers,
-            body: data
+    if (newUser.checked) {
+      fetch("http://192.168.1.208/users/", {
+        method: "POST",
+        headers: headers,
+        body: data,
+      })
+        .then(function (response) {
+          return response.json();
         })
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                if (data.access_token) {
-                    console.log(data.access_token)
-                    localStorage.setItem("token", data.access_token)
-                    window.open("index.html")
-                }
-                else {
-                    window.alert("Invalid Login")
-                }
-            });
+        .then(function (data) {
+          console.log(data);
+        });
+      window.alert("New User Saved! Logging in...");
+    }
+
+    setTimeout(console.log("Logging in..."), 1500);
+
+    fetch("http://192.168.1.208/login/", {
+      method: "POST",
+      headers: headers,
+      body: data,
     })
-})
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.access_token) {
+          console.log(data.access_token);
+          localStorage.setItem("token", data.access_token);
+          window.open("index.html");
+        } else {
+          window.alert("Invalid Login");
+        }
+      });
+  });
+});
