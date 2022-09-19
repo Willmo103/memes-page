ab16a0b8898dcc917a868c78cafc07ab89c5ed17
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2022-09-19 13:55:18 -0500

    small fixes to code for redirects

--- login.js
+++ login.js
@@ -3,12 +3,11 @@ window.addEventListener("load", () => {
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-
     let email = document.querySelector("input[name=email]").value;
     let password = document.querySelector("input[name=password]").value;
     let newUser = document.querySelector("input[name=newUser]");
     let data = JSON.stringify({ email: email, password: password });
-    console.log(data);
+    // console.log(data);
     const headers = {
       "Content-Type": "application/json",
       "Access-Control-Origin": "*",
@@ -21,12 +20,19 @@ window.addEventListener("load", () => {
         body: data,
       })
         .then(function (response) {
-          return response.json();
+          if (response.ok) {
+            return response.json();
+          } else {
+            window.alert("Email already in use!");
+            return;
+          }
         })
         .then(function (data) {
-          console.log(data);
+          if (data) {
+            console.log("New User Created\n", data);
+            window.alert("New User Saved! Logging in...");
+          }
         });
-      window.alert("New User Saved! Logging in...");
     }
 
     setTimeout(console.log("Logging in..."), 1500);
@@ -41,9 +47,9 @@ window.addEventListener("load", () => {
       })
       .then(function (data) {
         if (data.access_token) {
-          console.log(data.access_token);
+          //   console.log(data.access_token);
           localStorage.setItem("token", data.access_token);
-          window.open("index.html");
+          window.location.href = "index.html";
         } else {
           window.alert("Invalid Login");
         }
--- mymemes.js
+++ mymemes.js
@@ -1,3 +1,110 @@
-window.addEventListener("load", () => {
+function getUserMemes() {
+  let memeArr = [];
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Origin": "*",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
+  fetch(`http://192.168.1.208/save/`, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (i in data.memes) {
+        console.log(data.memes[i]);
+        memeArr.push(data.memes[i]);
+      }
+      return memeArr;
+    });
+  console.log(memeArr);
+  return memeArr;
+}
 
-})
+function logout() {
+  localStorage.clear();
+  window.location.href("index.html");
+}
+
+window.addEventListener("load", function () {
+  if (!localStorage.getItem("token")) {
+    window.location.href = "index.html";
+  }
+  let current = 0;
+  let memes = getUserMemes();
+  let token = localStorage.getItem("token");
+  const img = document.getElementById("image");
+  const backBtn = document.getElementById("back");
+  const nextBtn = document.getElementById("next");
+  const linksDiv = document.getElementById("links");
+  const deleteBtn = document.getElementById("save");
+
+  if (current <= 0) {
+    backBtn.style.visibility = "none";
+    nextBtn.style.visibility = "visible";
+  } else if (current >= memes.length) {
+    backBtn.style.visibility = "visible";
+    nextBtn.style.visibility = "none";
+  }
+
+  if (token) {
+    linksDiv.innerHTML = `
+        <li class="nav-item">
+        <a class="nav-link" id="home" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" id="logout" href="" onclick="logout()">Logout</a>
+        </li>`;
+  } else {
+    linksDiv.innerHTML = `<a class="nav-link" id="loginLink" href="login.html">Login</a>`;
+  }
+
+  setTimeout(function () {
+    console.log(memes);
+    img.src = memes[current].url;
+  }, 1000);
+
+  backBtn.addEventListener("click", function () {
+    current -= 1;
+    if (current < 0) {
+      current = 0;
+    } else {
+      img.src = memes[current].url;
+    }
+  });
+
+  nextBtn.addEventListener("click", function () {
+    current += 1;
+    if (current >= memes.length - 1) {
+      let moreMemes = getUserMemes();
+      setTimeout(function () {
+        memes = memes.concat(moreMemes);
+      }, 1000);
+    }
+
+    if (current !== memes.length) {
+      img.src = memes[current].url;
+    }
+  });
+
+  deleteBtn.addEventListener("click", function () {
+    let memeId = memes[current].id;
+    console.log(memeId);
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Origin": "*",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    };
+    fetch(`http://192.168.1.208/save/${memeId}`, {
+      method: "DELETE",
+      headers: headers,
+    }).then(function (response) {
+      if (response.ok) {
+        window.alert("Meme Deleted!");
+        location.reload();
+      }
+    });
+  });
+});
